82b63e6b1cae9d7b2ab1a83719490051df005c46
Wyller <wyller22@gmail.com>
2023-03-05 22:38:01 -0300

    Cleaning the code

--- lib/widgets/async_city_form.dart
+++ lib/widgets/async_city_form.dart
@@ -20,112 +20,140 @@ class _AsyncCityFormState extends State<AsyncCityForm> {
     return DefaultTextStyle(
       style: Theme.of(context).textTheme.displayMedium!,
       textAlign: TextAlign.center,
-      child: FutureBuilder<String>(
-        future: _brazilCities, // a previously-obtained Future<String> or null
-        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-          Widget rendered;
+      child: _futureBuilder(),
+    );
+  }
 
-          if (snapshot.hasData) {
-            rendered = BlocProvider(
-              create: (context) => AsyncCityFormBloc(),
-              child: Builder(builder: (context) {
-                final formBloc = context.read<AsyncCityFormBloc>();
+  FutureBuilder<String> _futureBuilder() {
+    return FutureBuilder<String>(
+      future: _brazilCities,
+      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+        Widget rendered;
+        if (snapshot.hasData) {
+          rendered = BlocProvider(
+            create: (context) => AsyncCityFormBloc(),
+            child: Builder(builder: (context) {
+              final formBloc = context.read<AsyncCityFormBloc>();
+              return _scaffoldSearchForm(formBloc);
+            }),
+          );
+        } else if (snapshot.hasError) {
+          rendered = _scaffoldErrorOcurred(snapshot);
+        } else {
+          rendered = _scaffoldLoadingCities();
+        }
+        return rendered;
+      },
+    );
+  }
 
-                return Scaffold(
-                  resizeToAvoidBottomInset: false,
-                  appBar: AppBar(title: const Text('⚒️  Previsões CPTEC MVP')),
-                  body: FormBlocListener<AsyncCityFormBloc, String, String>(
-                    onSubmitting: (context, state) {
-                      LoadingDialog.show(context);
-                    },
-                    onSubmissionFailed: (context, state) {
-                      LoadingDialog.hide(context);
-                    },
-                    onSuccess: (context, state) {
-                      LoadingDialog.hide(context);
-                      final cityName = formBloc.cityName.value;
-                      getCityForecasts(cityName).then((value) {
-                        Navigator.of(context).pushReplacement(MaterialPageRoute(
-                            builder: (_) => ForecastResults(value)));
-                      });
-                    },
-                    onFailure: (context, state) {
-                      LoadingDialog.hide(context);
-
-                      ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text(state.failureResponse!)));
-                    },
-                    child: formLayout(formBloc),
-                  ),
-                );
-              }),
-            );
-          } else if (snapshot.hasError) {
-            rendered = Column(
-              children: <Widget>[
-                const Icon(
-                  Icons.error_outline,
-                  color: Colors.red,
-                  size: 60,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16),
-                  child: Text('Error: ${snapshot.error}'),
-                ),
-              ],
-            );
-          } else {
-            rendered = Column(
-              children: const <Widget>[
-                SizedBox(
-                  width: 60,
-                  height: 60,
-                  child: CircularProgressIndicator(),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 16),
-                  child: Text('Awaiting result...'),
-                ),
-              ],
-            );
-          }
-          return rendered;
-        },
+  Scaffold _scaffoldErrorOcurred(AsyncSnapshot<String> snapshot) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          const Icon(
+            Icons.error_outline,
+            color: Colors.red,
+            size: 60,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16),
+            child: Text('Erro: ${snapshot.error}'),
+          ),
+        ],
       ),
     );
   }
 
-  SingleChildScrollView formLayout(AsyncCityFormBloc formBloc) {
+  Scaffold _scaffoldLoadingCities() {
+    return Scaffold(
+      body: Column(
+        children: const <Widget>[
+          SizedBox(
+            width: 60,
+            height: 60,
+            child: CircularProgressIndicator(),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 16),
+            child: Text('Carregando municípios...'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Scaffold _scaffoldSearchForm(AsyncCityFormBloc formBloc) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: const Text('⚒️  Previsões CPTEC MVP'),
+      ),
+      body: FormBlocListener<AsyncCityFormBloc, String, String>(
+        onSubmitting: (context, state) {
+          LoadingDialog.show(context);
+        },
+        onSubmissionFailed: (context, state) {
+          LoadingDialog.hide(context);
+        },
+        onSuccess: (context, state) {
+          LoadingDialog.hide(context);
+          final cityName = formBloc.cityName.value;
+          getCityForecasts(cityName).then((value) {
+            Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (_) => ForecastResults(value)));
+          });
+        },
+        onFailure: (context, state) {
+          LoadingDialog.hide(context);
+
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text(state.failureResponse!)));
+        },
+        child: _searchFormScrollView(formBloc),
+      ),
+    );
+  }
+
+  SingleChildScrollView _searchFormScrollView(AsyncCityFormBloc formBloc) {
     return SingleChildScrollView(
       physics: const ClampingScrollPhysics(),
       child: Column(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFieldBlocBuilder(
-              textFieldBloc: formBloc.cityName,
-              suffixButton: SuffixButton.asyncValidating,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                labelText: 'Nome do Município',
-                prefixIcon: Icon(Icons.location_city),
-              ),
-            ),
-          ),
-          ElevatedButton(
-            onPressed: formBloc.submit,
-            child: const Padding(
-              padding: EdgeInsets.all(12.0),
-              child: Text(
-                '☁️ Obter previsões',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.normal,
-                ),
-              ),
-            ),
-          ),
+          _searchFormInput(formBloc),
+          _searchFormSubmitButton(formBloc),
         ],
+      ),
+    );
+  }
+
+  ElevatedButton _searchFormSubmitButton(AsyncCityFormBloc formBloc) {
+    return ElevatedButton(
+      onPressed: formBloc.submit,
+      child: const Padding(
+        padding: EdgeInsets.all(12.0),
+        child: Text(
+          '☁️ Obter previsões',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding _searchFormInput(AsyncCityFormBloc formBloc) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFieldBlocBuilder(
+        textFieldBloc: formBloc.cityName,
+        suffixButton: SuffixButton.asyncValidating,
+        keyboardType: TextInputType.emailAddress,
+        decoration: const InputDecoration(
+          labelText: 'Nome do Município',
+          prefixIcon: Icon(Icons.location_city),
+        ),
       ),
     );
   }
